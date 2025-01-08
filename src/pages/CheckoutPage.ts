b59ca0ae01474c35bd35b16d3178ba5295d971e1
Dileep Kumar <samudsan2020@gmail.com>
2025-01-08 16:59:00 +0530

    Fixed checkout page test failutes

--- src/pages/CheckoutPage.ts
+++ src/pages/CheckoutPage.ts
@@ -1,12 +1,22 @@
 import { BasePage } from './BasePage';
 
 export class CheckoutPage extends BasePage {
-  private cartItemSelector = '.cart-item';
+  private cartItemSelector = '.cart_item';
   private checkoutButtonSelector = '#checkout';
+  private checkoutLink = ".shopping_cart_link";
+  private addItem = "[data-test=add-to-cart-sauce-labs-backpack]";
 
   async verifyCartItemsCount(expectedCount: number): Promise<boolean> {
     const items = await this.page.$$(this.cartItemSelector);
     return items.length === expectedCount;
+  }
+
+  async addItemToCart() : Promise<void>{
+    await this.page.locator(this.addItem).click();
+  }
+
+  async clickOnCartLink(){
+    await this.page.locator(this.checkoutLink).click();
   }
 
   async proceedToCheckout() {
--- src/tests/checkout.test.ts
+++ src/tests/checkout.test.ts
@@ -4,15 +4,17 @@ import { CheckoutPage } from '../pages/CheckoutPage';
 test.describe('Checkout Page Tests', () => {
   test('Verify cart item count', async ({ page }) => {
     const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.navigateTo('/checkout');
-    const isCartCountValid = await checkoutPage.verifyCartItemsCount(3);
+    await checkoutPage.navigateTo('inventory.html');
+    await checkoutPage.addItemToCart();
+    await checkoutPage.clickOnCartLink();
+    const isCartCountValid = await checkoutPage.verifyCartItemsCount(1);
     expect(isCartCountValid).toBeTruthy();
   });
 
   test('Proceed to checkout', async ({ page }) => {
     const checkoutPage = new CheckoutPage(page);
-    await checkoutPage.navigateTo('checkout');
+    await checkoutPage.navigateTo('cart.html');
     await checkoutPage.proceedToCheckout();
-    expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
+    expect(page.url()).toBe('https://www.saucedemo.com/checkout-step-one.html');
   });
 });
